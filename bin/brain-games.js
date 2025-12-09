f79e172a5136c64d649cb14e0d8a83b048e7f11d
Sergey Vorontsov <sevor005@yandex.ru>
2025-12-09 21:32:30 +0700

    Настройка линтинга.

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 
-import { runGame } from '../src/cli.js';
+import { runGame } from '../src/cli.js'
 
-runGame();
+runGame()
--- src/cli.js
+++ src/cli.js
@@ -4,6 +4,6 @@ export const runGame = () => {
   console.log('Welcome to the Brain Games!');
 
   const name = readlineSync.question('May I have your name? ');
-  
+
   console.log(`Hello, ${name}`);
 }
